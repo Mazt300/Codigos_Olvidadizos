36b84c9a1c4644bcd9a03cf362d585492bf3399c
Mazt300 <mazt.frouh@gmail.com>
2021-04-20 17:41:39 -0600

    update

--- Codigos_Olvidadizos/Web/Contextos/Contextos.cs
+++ Codigos_Olvidadizos/Web/Contextos/Contextos.cs
@@ -1,6 +1,6 @@
 ﻿    public class DbContext : DbContext
     {
-        public DbContext() : base("Server=tcp:almacendedatos.database.windows.net,1433;Initial Catalog = BD; Persist Security Info=False;User ID = administradorglobal; Password=Cs_almacendatos; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 300;")
+        public DbContext() : base("Conexion")
         {
         }
 
